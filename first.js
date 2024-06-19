264bff7178f62498172f479387245eb09523ace6
Het Mardiya <hetmardiya9@gmail.com>
2024-06-19 15:33:52 +0530

    two functions created

---
+++ first.js
@@ -0,0 +1,12 @@
+function sum(a=0,b=0,c=0) {
+    let result = a + b + c;
+    return result;
+}
+
+console.log(sum(10,10,10));
+
+function sum1(a,b,c){
+    let result = a + b + c;
+    console.log(result);
+}
+sum1(20,20,20)
