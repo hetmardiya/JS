896b88882772829a0df125b24fb6003960a52b86
Het Mardiya <hetmardiya9@gmail.com>
2024-07-11 19:15:15 +0530

    add a parent

--- first.js
+++ first.js
@@ -550,3 +550,13 @@ function totalOfArray(arr=[]){
 // (function abc(){
 //     console.log("hello");
 // })();
+
+let parent = document.querySelector('.parent')
+// console.log(parent);
+// console.log(parent.children);
+// console.log(parent.children[2].innerHTML);
+
+for (let i = 0; i < parent.children.length; i++) {
+    console.log(parent.children[i].innerHTML);
+    
+}
